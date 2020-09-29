50009a55c9c139ef92c5bc32afee399ea515bf20
John <63030903+johnmyronmillar@users.noreply.github.com>
2020-09-28 23:49:11 -0500

    update to new code

--- SolidityCode/AssociateProfitSplitter.sol
+++ SolidityCode/AssociateProfitSplitter.sol
@@ -9,9 +9,9 @@ contract AssociateProfitSplitter {
     // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
     using SafeMath for uint;
     
-    address employee_one;
-    address employee_two;
-    address employee_three;
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;
 
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
@@ -25,25 +25,22 @@ contract AssociateProfitSplitter {
 
     function deposit() public payable {
         // @TODO: Split `msg.value` into three
-        uint amount = msg.value.div(3); // Your code here!
+        uint amount = msg.value.div(3);
 
         // @TODO: Transfer the amount to each employee
-        // Your code here!
-        balances[employee_one] = balances[employee_one].add(amount);
-        balances[employee_two] = balances[employee_two].add(amount);
-        balances[employee_three] = balances[employee_three].add(amount);
+        employee_one.transfer(amount);
+        employee_two.transfer(amount);
+        employee_three.transfer(amount);
 
         // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
         uint remainder = msg.value.sub(amount * 3);
-        balances[msg.sender] = balances[msg.sender].add(remainder);
+        employee_one.transfer(remainder);
+        // balances[msg.sender] = balances[msg.sender].add(remainder);
 
     }
 
     function() external payable {
         // @TODO: Enforce that the `deposit` function is called in the fallback function!
-        // Your code here!
         deposit;
-        
     }
 }
